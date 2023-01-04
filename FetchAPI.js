383043975698c2c7b5f09d13c327fd2627dbc1a5
Rafał <115103860+RafalGolasz@users.noreply.github.com>
2023-01-04 22:37:30 +0100

    Task-3
    Fetch

---
+++ FetchAPI.js
@@ -0,0 +1,23 @@
+import fetch from 'unfetch';
+
+  fetch('http://localhost/portal/api/posts')
+  .then(response => response.json())
+  .then(posts => {
+    
+    const container = document.getElementById('posts-container');
+
+    const postElements = posts.map(post => {
+      const postElement = document.createElement('div');
+      postElement.innerHTML = `
+        <h2>${post.title}</h2>
+        <p>${post.body}</p>
+      `;
+      return postElement;
+    });
+
+    postElements.forEach(element => container.appendChild(element));
+  });
+
+
+
+  
