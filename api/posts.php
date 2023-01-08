e217b01319ea6a8de2ea1f6a02c01c04d70b60c0
Krzysztof Bicz <kristofarer@gmail.com>
2023-01-08 22:13:05 +0100

    Cameleons handling
    Not finished yet, but i found a way to do it properly

--- api/posts.php
+++ api/posts.php
@@ -6,7 +6,7 @@ include '../accounts/connection.php';
 
 $postArray = [];
 
-$sql = "SELECT dane_podstawowe.imie AS imie, dane_podstawowe.nazwisko AS nazwisko, posts.PostDate AS PostDate, posts.Post AS Post, posts.Cameleons AS Cameleons FROM dane_podstawowe JOIN posts ON dane_podstawowe.id = posts.userID";
+$sql = "SELECT dane_podstawowe.imie AS imie, dane_podstawowe.nazwisko AS nazwisko, posts.ID AS id, posts.PostDate AS PostDate, posts.Post AS Post, posts.Cameleons AS Cameleons FROM dane_podstawowe JOIN posts ON dane_podstawowe.id = posts.userID";
 
 $result = $connection -> query($sql);
 
@@ -15,8 +15,9 @@ if($result -> num_rows > 0)
     while($row = $result -> fetch_assoc())
     {
         $post = [
-            "imie" => $row['imie'],
-            "nazwisko" => $row['nazwisko'],
+            "postId" => $row['id'],
+            "name" => $row['imie'],
+            "surname" => $row['nazwisko'],
             "postDate" => $row['PostDate'],
             "post" => $row['Post'],
             "cameleons" => $row['Cameleons']
---
+++ main_page.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == false)
+{
+    header('location: index.html');
+}
+?>
+
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+    <meta charset="utf-8" />
+    <Title> Cameleon - Najlepszy portal! </title>
+    <meta name= "description" content="Serwis prezentuje portal spolecznosciowy. Sprawdz go odrazu, nie zwlekaj" />
+    <meta name= "keywords" content="portal, portal spolecznosciowy, znajomi, posty" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+</head>
+
+<body>
+
+<header>
+
+    <div>
+
+    </div>
+
+    <div>
+        <nav>
+
+        </nav>
+    </div>
+
+</header>
+
+<main>
+    <div id="posty">
+
+    </div>
+</main>
+
+<script>
+
+</script>
+
+</body>
+</html>
